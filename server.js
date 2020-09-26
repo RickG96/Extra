95624c6349f046cb343b44f35b34881de31e0b7c
Ricardo <rick96_@hotmail.com>
2020-09-26 02:42:42 -0600

    conexion push

--- server.js
+++ server.js
@@ -6,7 +6,27 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
 const adapter = new FileSync('db.json');
-const db = low(adapter)
+const db = low(adapter);
+
+var admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        "type": "service_account",
+        "project_id": "smartcarweb-74525",
+        "private_key_id": "6589ad5dd678810d3a395632f844ddbc5a9bab3e",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDEkUbFA5NYbUgk\nvu45xxLMj5OnKbyHz4MqvQG0dz74iAxiLZGWb2K2j9UOxjXeg6wcN09c7vnM8Urc\nGkTxrETcyS1H/3P/+MWL2Otk0M3+rSHylS4vUODnyDktyPUopbMQJR7iEQFmTewz\nJRFdKH3foK7PhH/rufEUJW8nz5FVfSM7feBBVx2y7hi5c+CR4yZmyvmD/Z0Sud2m\ng/HVNIPwVsgw0jRoB1Wq3ggx6UaCTBG69JqllnPHM7xVjs+miSXBAazXcJmm+FPw\nkxpCJqfDU+Sqd+tA5zUCVMpgNDlAhd+JTmqLBNUh+D0e/UuBwWBVK6LHDYzmwQ9K\neLvOoxPzAgMBAAECggEAH7zLHeVBZZ4jb1dqlGaZmPC3qN5aw9zT81MYEuDMCInC\n4hk0cAMk/5ACmQOU0AwXfmjJxhoJuDbfXOFa72Ber/1Fc+IBgztR/g30l88E65Fd\nsVK35cjbv2C7JrLTLpBudHVh4qNXRALh17Is9wsiI7jIcNYWZ2Oa0qNmqplfgFtP\nCEhkvBzdW71oV8OiUP10AKS0F6dNuMpHtLNbPPV3P8tqGFXmsXYyKUiJDzKmSHEp\nHuzCoij4BqwggofHDs9IcQoMZam5BmQ1l8PEFpCXxO4pEaFf6jufZPNsV4OSCx0P\nUIMobwMIg9ByDp7NW1WH5D/XZSqsMZCINaRHXuCinQKBgQDs8h7Hy8X+kRpDVptJ\nPbXLr8cpzzv+uZKnPnVzTCsr/C8/uESgYnroRYB1hLlbmUxM6b3p0BCchkxu5uuf\nZ3FnuxPRPLWSXea1MU+oqaIk/aEFndk31omWGyzm3WeHq28a62do3VjjvbOIIhl+\nvKYRwYZrD88tHbOHE4/ZMCxvbQKBgQDUX+jBFeFoRxQZxqrpmDz1iWfhdIm/WHB+\nZMn+KKJGkIwVItMb0jzN/NilvC6QMhaIdx8ELnRVI6Z4865KfnJHdO145z0TBC9q\nFEgavxPAICWF0V9KGZumH3IVAv08Yt+fF2nItiPbEKhizVRY2FfL5xT8kDTSPMyz\nKk4rbK+U3wKBgEg+cmAco1vAxi8t4t7r79gQ2jrHMtWxoyNXCdyDps4ccBOPjW0R\nWcQkHMy4EQP6s6bnxb3acJYo5HkT81eA5LTp91relcXw13z7cfxM7GE267eYrzg1\nM3IPCxvA+eYFwTSF/dudUZLuriSdsQRRZJkZVnCiO1rYKXrP3hYsd//pAoGAUyYV\n3/ssHIKvuQPSiNDFFTEuDdRt1g/PQ308pJjOKCiR3/iZhOwESHX1cLZD2MrzjYxu\nBVoWy/rAQ9zMYXguUBHUpCCNBfP+iF7WeDpVhkRPzJvT3hQ2n4zyQo3ADjN78DLf\nhOjjEvLrqntPydUyUq/vxI7FT4bkckVfA5tzBysCgYBPT9qerV3szCVzFn5is9GO\n0x/iyi/BckIZz+kn6rZ4ETfuNAinW0SWq5mOnTBVT4vvrPEl5iiyH+DrwZcVpSfQ\nbwCZWWuP2TOBs9Aj5vKwTXyKh2Wy69cQGVEjo2Tv5jmsJhBuu8YRwrqEcUbF9fs9\n/xs6utG92LPtaRmRVCDFvw==\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-tmkly@smartcarweb-74525.iam.gserviceaccount.com",
+        "client_id": "116905074636670847755",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-tmkly%40smartcarweb-74525.iam.gserviceaccount.com"
+      }),
+      databaseURL: "https://smartcarweb-74525.firebaseio.com"
+});
+const dba = admin.firestore();
+const messaging = admin.messaging();
 
 db.defaults({ peso: [], hora_salida: [], hora_llegada: [], t_entrega: [], t_regreso: [], obstaculos: 0 })
     .write();
@@ -14,6 +34,8 @@ db.defaults({ peso: [], hora_salida: [], hora_llegada: [], t_entrega: [], t_regr
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+var tokenFB = "AAAAr88BvCs:APA91bEo_Ag6w_kL8_DEqa-eR9WKXgLQkl6bBVO1oHZjSFzssKGCF1f4FJhamoBD3ZP64JcW0H7jL7LVS5GFAi7Or2h4pc-UG7FY60W3J-4xklFgm5nzYnYrjivdZGArbQLG-IIjsa3l";
 
 var actual = -1;
 var nsalidas = 0;
@@ -64,6 +86,27 @@ app.post('/setestados', (req, res) => {
                   .write();
                   datosCarro.obstaculos = datos[3];
             } 
+
+            var message = {
+                notification: {
+                    title: 'Esperando',
+                    body: 'El carrito se encuentra esperando un paquete'
+                },
+                android: {
+                    notification: {
+                      icon: 'stock_ticker_update',
+                      color: '#7e55c3'
+                    }
+                },
+                token: tokenFB
+            };
+            admin.messaging().send(message)
+                .then((res) => {
+                    console.log('Notificación de buzon enviada con exito!');
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
         }
     } else if(datos[0] == 1) {
         if(actual != datos[0] && parseInt(datos[2]) > 0) {
@@ -163,12 +206,13 @@ app.get('/getestados', (req, res) => {
 });
 
 app.get('/getestadoactivo', (req, res) => {
+    console.log('Enviando a carrito...');
     if(mCarrito == "") {
         res.send("s");
     } else {
         res.send("n");
     }
-})
+});
 
 app.get('/promedio', (req, res) => {
     promedioPeso = 0;
