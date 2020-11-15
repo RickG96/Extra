eaeeab60689ce07ef371c06c1f7b25013e03a1ab
Ricardo <rick96_@hotmail.com>
2020-11-14 23:38:44 -0600

    servicios

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-let enviar;
+db.defaults({ posts: [], visitas: 0, temperatura: [], tiempo: [] })
+    .write();
+
+let enviar = {};
 
 app.post('/infoed3', (req, res) => {
    if(req.body.data == null) {
@@ -23,11 +31,64 @@ app.post('/infoed3', (req, res) => {
            fase: datos[4],
        }
        console.log(enviar);
+       db.get('posts')
+        .push(enviar)
+        .write();
    }
    
     res.json('ok'); 
 });
 
+app.get('/visitante', (req, res) => {
+    db.update('visitas', n => n + 1)
+        .write();
+    
+    res.json('vistas totales: ' + db.get('visitas').value());
+});
+
+app.post('/temperatura', (req, res) => {
+    db.get('temperatura')
+        .push(req.body.temperatura)
+        .write();
+    res.json('ok');
+});
+
+app.post('/tiempo', (req, res) => {
+    db.get('tiempo')
+        .push(req.body.tiempo)
+        .write();
+    res.json('ok');
+});
+
+app.get('/gettemperatura', (req, res) => {
+    let temp = 0;
+    db.get('temperatura').value().forEach(element => {
+        temp = temp + element;
+    });
+    temp = temp / db.get('temperatura').size().value();
+    let openload ={
+        temperatura: temp.toFixed(2)
+    }
+    res.json(openload);
+});
+
+app.get('/gettiempo', (req, res) => {
+    let tiempo_t = 0;
+    db.get('tiempo').value().forEach(element => {
+        tiempo_t = tiempo_t + element;
+    });
+    tiempo_t = tiempo_t / db.get('tiempo').size().value();
+    let openload = {
+        tiempo: tiempo_t.toFixed(2)
+    }
+    res.json(openload);
+});
+
+app.get('/getvisitas', (req, res) => {
+    let numeroVisitas = db.get('visitas').value();
+    let openload = {vistas: numeroVisitas + ''}
+    res.json(openload);
+})
 
 app.get('/getinfoed3', (req, res) => {
     res.json(enviar);
